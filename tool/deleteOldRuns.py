95eb2d2c05537804e9153f50782861a929ae823b
zhzjwd <zhzjasd@gmail.com>
2021-03-13 22:55:23 +0800

    update deleteOldRuns.py

--- tool/deleteOldRuns.py
+++ tool/deleteOldRuns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-import os
+import os,sys
 import requests
 
 class GitActionCleaner():
@@ -39,7 +39,7 @@ class GitActionCleaner():
             self.help()
             e = str(e).strip("'")
             print(f'----------\n需要设置环境变量 {e}: {self.envInfo[e]}\n----------')
-            os._exit(0)
+            sys.exit(0)
         # 获取所有运行记录
         self.getAllRuns()
         # 删除已取消
